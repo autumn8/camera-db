9fc2cbc9cde019cf9dab9612da80dc9950eaa44b
Brad Roodt <brad.roodt@gmail.com>
2019-05-19 20:25:58 +0200

    feat: simplified find or create logic

--- app.js
+++ app.js
@@ -13,62 +13,44 @@ client.on('connect', function () {
 
 client.on("message", (topic, message) => {
     console.log("topic", topic);
-    if (topic.includes("camera/update")){
+    if (topic.includes("camera/update")) {
         console.log("camera update");
         const cameraProps = JSON.parse(message);
         console.log(JSON.parse(message));
         const routeSegments = topic.split("/");
         const cameraName = routeSegments[routeSegments.length - 1];
-        Camera.findOneAndUpdate({name: cameraName}, cameraProps, function(err, doc){
+        Camera.findOneAndUpdate({ name: cameraName }, cameraProps, function (err, doc) {
             if (err) console.warn(err);
             console.log('updated succesfully, send update to mqtt broker');
         });
     }
-    else if (topic.includes("camera/connected")) {        
+    else if (topic.includes("camera/connected")) {
         //if messge is 1- device is connectedconnected and payload is 1, device has connected to broker.
         if (message.toString() == 1) {
-            const routeSegments = topic.split("/");
-            const cameraName = routeSegments[routeSegments.length - 1];
-            //See if camera has already been initialized (exists in db)   
-            //replace with find one and update     Camera.findOneAndUpdate({name: cameraName}, cameraProps, {upsert:true}, function(err, doc){               
-            Camera.find({ name: cameraName })
-                .then(cameras => {
-                    if (cameras.length) handleCameraAlreadyInitialized(cameras, cameraName);
-                    else handleCameraNotInitialized(cameras, cameraName)
-                })
-                .catch(error => console.log(error));
+            onCameraConnected(topic, message);
         }
     }
 });
 
-function handleCameraNotInitialized(cameras, cameraName) {    
-    const cameraWithDefaultValues = generateCameraDefaultValues(cameraName);
-    cameraWithDefaultValues.save(function (err, camera) {
-        if (err) return console.error(console.log(camera));
-        console.log('created');
-        console.log(camera);
-        client.publish(`camera/initialized/${camera.name}`, JSON.stringify(camera));
-    });
-}
 
-function handleCameraAlreadyInitialized(cameras) {
-    //caution if more than 1 device is found. 
-    if (cameras.length > 1) console.warn('CAUTION! More than 1 camera found. Returning first camera');
-    const camera = cameras[0];
-    console.log('found');
-    console.log(camera);
-    client.publish(`camera/initialized/${camera.name}`, JSON.stringify(camera));
-}
-
-function generateCameraDefaultValues(cameraName) {
-    console.log(cameraName);
-    return new Camera({
+function onCameraConnected(topic, message) {
+    const routeSegments = topic.split("/");
+    const cameraName = routeSegments[routeSegments.length - 1];
+    const defaults = {
         name: cameraName,
-        ...cameraDefaults 
-    });
+        ...cameraDefaults
+    };
+
+    Camera.findOneAndUpdate({ name: cameraName }, {$setOnInsert: defaults}, { upsert: true})
+        .then(camera => {
+            console.log(camera);
+            const cameraSettings = JSON.stringify(camera)
+            client.publish(`camera/initialized/${camera.name}`, cameraSettings);
+        })
+        .catch(error => console.log(error));
 }
 
-mongoose.connect('mongodb://localhost/camera', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/camera', { useNewUrlParser: true, useFindAndModify: false });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
